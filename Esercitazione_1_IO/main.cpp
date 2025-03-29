cb0315cda9679756d5fd762cc639abc3ace6d5ac
Giulia De Santis <ingegneriagiulia@gmail.com>
2025-03-29 10:32:58 +0100

    my commit

--- Esercitazione_1_IO/main.cpp
+++ Esercitazione_1_IO/main.cpp
@@ -10,13 +10,13 @@ double map_values(double x){
 	int b = 5;
 	int c = -1;
 	int d = 2;
-	return ((c-d)*x+a*d-b*c)/(a-b); // la formula da [a,b] a [c,d] sarebbe: x' = [(c-d)*x+a*d-b*c]/(a-b)
+	return ((c-d)*x+a*d-b*c)/(a-b); // la formula per passare da [a,b] a [c,d] è: x' = [(c-d)*x+a*d-b*c]/(a-b)
 }
 
 int main()
 {
 	std::ifstream file("data.txt");
-	if (file.fail()) // per controllare se il file si apre correttamente
+	if (file.fail()) // per controllare se il file di input si apre correttamente
 	{
 		std::cerr << "errore nell'apertura del file" << std::endl;
 		return 1;
@@ -32,13 +32,13 @@ int main()
 
 	double val; //creo variabile ausiliaria dove vado a memorizzare i valori letti dal file
 	double mean = 0.0;
-	unsigned int k = 0;
+	unsigned int k = 1;
 	double x = 0.0;
 	ofs << "# N Mean" << std::endl;
 	while (file >> val) { 
-        	x = map_values(val);
-		mean = (mean * k + x)/(k+1);
-		ofs << std::scientific << std::setprecision(16) << mean << std::endl;
+        x = map_values(val);
+		mean = (mean * (k-1) + x)/k;
+		ofs << k << " " << std::scientific << std::setprecision(16) << mean << std::endl;
 				
 		k++;
 	}
